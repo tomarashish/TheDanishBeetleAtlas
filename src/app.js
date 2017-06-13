bf81c9bc58202fa17b607ed7dc564a55c0b5fbfe
tomarashish <aashish.dyp@gmail.com>
2017-06-13 09:08:20 +0200

    add centroid point function

--- src/app.js
+++ src/app.js
@@ -21,14 +21,42 @@ d3.json("../data/denmark-counties.json", function(error, map) {
 
   	console.log(map)
   
-  svg.append("g")
-      .attr("class", "states")
-    .selectAll("path")
+  var groups = svg.selectAll("g")
     .data(topojson.feature(map, map.objects.denmarktopo).features)
-    .enter().append("path")
+    .enter().append("g");
+  
+  
+      groups.append("path")
       .attr("d", path)
     .style("fill", "none")
     .style("stroke", "#575757")
       	.style("stroke-width", "2.5");
+  
+  
+    //add circular points
+    groups.each(function(d,i){
+        
+      var center = projection(d3.geoCentroid(d)); 
+      
+      d3.select(this)
+        .append("circle")
+        .attr("cx", center[0])
+        .attr("cy", center[1])
+        .attr("r", "9px")
+        .style("fill","lightgrey")
+        .style("fill-opcity", "0.8")
+        .style("stroke", "#cccccc")
+      	.style("stroke-width", "0.5");
+      
+      d3.select(this)
+        .append("circle")
+        .attr("cx", center[0])
+        .attr("cy", center[1])
+        .attr("r", "5px")
+        .style("fill", "steelblue")
+        .style("stroke", "#575757")
+        .style("stroke-width", "0.5");
+      
+    })
 
-	});
+});
