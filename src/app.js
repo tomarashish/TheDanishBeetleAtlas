31a616b959c64342ab8e675bcbe669a346bd9442
tomarashish <aashish.dyp@gmail.com>
2017-06-13 10:11:15 +0200

    change topojson file

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ var svg = d3.select("#map").append("svg")
 	.attr("preserveAspectRatio", "xMidYMid");
 
 
-d3.json("../data/denmark-counties.json", function(error, map) {
+d3.json("../data/denmark.topo.json", function(error, map) {
 
   	console.log(map)
   
@@ -26,15 +26,18 @@ d3.json("../data/denmark-counties.json", function(error, map) {
     .enter().append("g");
   
   
-      groups.append("path")
+    groups.append("path")
       .attr("d", path)
-    .style("fill", "none")
-    .style("stroke", "#575757")
-      	.style("stroke-width", "2.5");
+      .style("fill", "none")
+      .style("stroke", "#575757")
+      .style("stroke-width", "2.5");
   
   
-    //add circular points
-    groups.each(function(d,i){
+  //http://bl.ocks.org/zanarmstrong/a40f50dc6a16844d5346
+  //http://bl.ocks.org/phil-pedruco/7745589
+  
+    //add circular points from centriod 
+    /*groups.each(function(d,i){
         
       var center = projection(d3.geoCentroid(d)); 
       
@@ -48,15 +51,21 @@ d3.json("../data/denmark-counties.json", function(error, map) {
         .style("stroke", "#cccccc")
       	.style("stroke-width", "0.5");
       
-      d3.select(this)
-        .append("circle")
-        .attr("cx", center[0])
-        .attr("cy", center[1])
-        .attr("r", "5px")
-        .style("fill", "steelblue")
-        .style("stroke", "#575757")
-        .style("stroke-width", "0.5");
-      
     })
+    */
+  
+  // fake cordinate points
+    aa = [-122.490402, 37.786453];
+	bb = [-122.389809, 37.72728];
+    //Add markers to map based on coordinates
+  // add circles to svg
+    groups.selectAll("circle")
+		.data([aa]).enter()
+		.append("circle")
+		.attr("cx", function (d) { console.log(projection(d)); return projection(d)[0]; })
+		.attr("cy", function (d) { return projection(d)[1]; })
+		.attr("r", "8px")
+		.attr("fill", "red")
+  
 
 });
