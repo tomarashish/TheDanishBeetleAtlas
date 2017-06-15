1c26b86ebd1039db59d63d5e618ccc6b536d39ed
tomarashish <aashish.dyp@gmail.com>
2017-06-15 16:41:23 +0200

    minor change

--- src/app.js
+++ src/app.js
@@ -21,15 +21,22 @@ var svg = d3.select("#map").append("svg")
 
 var hexbin = d3.hexbin()
     .extent([[0, 0], [width, height]])
-    .radius(10);
+    .radius(4);
 
 var radius = d3.scaleSqrt()
-    .domain([0, 12])
-    .range([0, 10]);
+    .domain([0, 15])
+    .range([0, 8]);
+
+var div = d3.select("body") 
+  .append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0)
+
+//https://bl.ocks.org/pstuffa/3393ff2711a53975040077b7453781a9
+var color = d3.scaleOrdinal(d3.schemeCategory20b);
+
 
 d3.json("../data/denmark.topo.json", function(error, map) {
-
-  	console.log(map)
   
   svg.selectAll("path")
     .data(topojson.feature(map, map.objects.denmarktopo).features)
@@ -38,34 +45,33 @@ d3.json("../data/denmark.topo.json", function(error, map) {
       .style("fill", "#f5eede")
       //.style("stroke", "#648d9e")
       .style("stroke-width", "2px");
-  
     
+  queue()
+    .defer(d3.csv, '../data/merge_data.csv')
+	.await(createMarker);
+  
+  //create map from combined data
+  function createMarker(error, atlasData ){
+  
+    console.log(atlasData)
     //https://github.com/proj4js/proj4js
     var utm = "+proj=utm +zone=32";
     var wgs84 = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
     
-  queue()
-	.defer(d3.csv, '../data/StednavneExxel.csv')
-	.defer(d3.csv, '../data/BillekatalogSamlet.csv')
-	.await(createMap);
-  
-  //create map from combined data
-  function createMap(error, cordsData, speciesData ){
-    console.log(cordsData[0])
     var cords = [];
     
-    for( var i = 0; i < cordsData.length; i++){
+    for( var i = 0; i < atlasData.length; i++){
+    //for( var i = 0; i < 100; i++){
       
       //console.log(proj4(utm,wgs84,[6246846,492837]));
       //console.log(proj4(utm,wgs84,[data[i].XKoord, data[i].YKoord]));
-      
-      cords.push(proj4(utm,wgs84,[cordsData[i].XKoord, cordsData[i].YKoord]))
+      if(atlasData[i].XKoord != "NA" || atlasData[i].YKoord != "NA")
+        cords.push(proj4(utm,wgs84,[atlasData[i].XKoord, atlasData[i].YKoord]))
     }//end of for loop
          
     
     //Add markers to map based on coordinates
   // add circles to svg
-    
     var circles = svg.selectAll("circle")
 		.data(cords).enter()
 		.append("circle")
@@ -74,20 +80,49 @@ d3.json("../data/denmark.topo.json", function(error, map) {
 		.attr("r", "1px")
         //.style("fill","#648d9e");
         .style("fill","#00485d");
+   // Modification of custom tooltip code provided by Malcolm Maclean, "D3 Tips and Tricks"
+	// http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
     
     
     //Add Zooming and panning
     
+    
     /*
     //Add hexagonal bins with hovering over shows tooltip with piechart
+    
+    hexabinData = cords.map(function(d){
+      var p = projection(d);
+      d[0] = p[0], d[1] = p[1];
+      
+      return d;
+    })
+    
      svg.append("g")
       .attr("class", "hexagon")
     .selectAll("path")
-    .data(hexbin(cordsData).sort(function(a, b) { return b.length - a.length; }))
+    .data(hexbin(hexabinData).sort(function(a, b) { return b.length - a.length; }))
     .enter().append("path")
       .attr("d", function(d) { return hexbin.hexagon(radius(2)); })
       .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-      */
+      .attr("fill", function(d) { return color(d3.median(d, function(d,i) { return i; })); });
+     
+    
+    
+    //Adding image marker with map
+    svg.selectAll(".mark")
+    .data(cords)
+    .enter()
+    .append("image")
+    .attr('class','mark')
+    .attr('width', 20)
+    .attr('height', 40)
+    .attr("xlink:href",'data/img/beetle1.png')
+    .attr("transform", function(d) {return "translate(" + projection([d[0],d[1]]) + ")";});
+    */
+    
+    //Heatmap 
+    //http://bl.ocks.org/kaijiezhou/82d0b794e845294b366e
+    
   }//end f cordinates csv
   
 });
