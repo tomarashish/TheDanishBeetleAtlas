0210c46e0005f41dd5a39d1d968c242daa92598b
tomarashish <aashish.dyp@gmail.com>
2017-06-26 10:53:06 +0200

    add slide drag

--- src/slider.js
+++ src/slider.js
@@ -1,67 +1,100 @@
+var margin = {right: 50, left: 50, top:20, bottom:50},
+  width = 700,
+    height= 50,
+    sliderWidth = 600;
 
-var data = d3.range(800).map(Math.random);
+var svg = d3.select("#slider").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+var xFirst = d3.scaleLinear()
+    .domain([1900, 2017])
+    .range([0, sliderWidth])
+    .nice();
+
+var xLast = d3.scaleLinear()
+    .domain([1900, 2017])
+    .range([0, sliderWidth])
+    .clamp(true);
+
+var sliderFirst = svg.append("g")
+    .attr("class", "slider")
+    .attr("transform", "translate(" + margin.left + "," +height/2 + ")");
+
+var sliderLast = svg.append("g")
+  .attr("class", "slider")
+  .attr("transform", "translate(" + margin.left + "," +height/2 + ")");
+
+sliderFirst.append("line")
+    .attr("class", "track")
+    .attr("id", "track1")
+    .attr("x1", xFirst.range()[0])
+    .attr("x2", xFirst.range()[1])
+  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-inset")
+  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-overlay")
+    .call(d3.drag()
+          .on("start.interrupt", function() {  sliderLast.interrupt(); })
+          .on("start drag", function() {moveSlideFirst(xFirst.invert(d3.event.x)); })
+         );
+
+sliderLast.append("line")
+  .attr("class", "track")
+  .attr("id", "track1")
+  .attr("x1", xLast.range()[0])
+      .attr("x2", xLast.range()[1])
+.select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-inset")
+  .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+    .attr("class", "track-overlay")
+    .call(d3.drag()
+         .on("start.interrupt", function() { sliderFirst.interrupt(); })
+        .on("start drag", function() { moveSlideLast(xLast.invert(d3.event.x)); })
+       );
 
 
-  var margin = {top: 194, right: 50, bottom: 214, left: 50},
-    width = 1200,
-    height = 800;
-    
-   var svgSlider = d3.select("#slider").append("g")
-      .attr("width", 1200)
-      .attr("height", 800)
-      .attr("viewBox", "0 0 900 800")
-	  .attr("preserveAspectRatio", "xMidYMid")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+sliderFirst.insert("g", ".track-overlay")
+    .attr("class", "ticks")
+    .attr("transform", "translate(0," + 18 + ")")
+  .selectAll("text")
+  .data(xFirst.ticks(10))
+  .enter().append("text")
+    .attr("x", xFirst)
+    .attr("text-anchor", "middle")
+    .style("fill", "#000")
+    .text(function(d) { return d ; }); 
 
-var x = d3.scaleLinear().range([0, width]),
-    y = d3.randomNormal(height / 2, height / 8);
+sliderLast//.insert("g", ".track-overlay")
+    .attr("transform", "translate(50," + 18 + ")")
+    .data(xLast.ticks(10));
 
-var brush = d3.brushX()
-    .extent([[0, 0], [width, height]])
-    .on("start brush end", brushmoved);
+var handle1 = sliderFirst.insert("circle", ".track-overlay")
+    .attr("class", "handle")
+    .attr("cx",xFirst.range()[0])
+    .attr("r", 9);
 
-svgSlider.append("g")
-    .attr("class", "axis axis--x")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+var handle2 = sliderLast.insert("circle", ".track-overlay")
+      .attr("class", "handle")
+      .attr("cx",xLast.range()[1])
+      .attr("r", 9);
 
-var circle = svgSlider.append("g")
-    .attr("class", "circle")
-  .selectAll("circle")
-  .data(data)
-  .enter().append("circle")
-    .attr("transform", function(d) { return "translate(" + x(d) + "," + y() + ")"; })
-    .attr("r", 3.5);
-
-var gBrush = svgSlider.append("g")
-    .attr("class", "brush")
-    .call(brush);
-
-var handle = gBrush.selectAll(".handle--custom")
-  .data([{type: "w"}, {type: "e"}])
-  .enter().append("path")
-    .attr("class", "handle--custom")
-    .attr("fill", "#666")
-    .attr("fill-opacity", 0.8)
-    .attr("stroke", "#000")
-    .attr("stroke-width", 1.5)
-    .attr("cursor", "ew-resize")
-    .attr("d", d3.arc()
-        .innerRadius(0)
-        .outerRadius(height / 2)
-        .startAngle(0)
-        .endAngle(function(d, i) { return i ? Math.PI : -Math.PI; }));
-
-gBrush.call(brush.move, [0.3, 0.5].map(x));
-
-function brushmoved() {
-  var s = d3.event.selection;
-  if (s == null) {
-    handle.attr("display", "none");
-    circle.classed("active", false);
-  } else {
-    var sx = s.map(x.invert);
-    circle.classed("active", function(d) { return sx[0] <= d && d <= sx[1]; });
-    handle.attr("display", null).attr("transform", function(d, i) { return "translate(" + s[i] + "," + height / 2 + ")"; });
+function moveSlideFirst(d) {
+    if (handle2.attr("cx") < (xFirst(d))) {
+      console.log("inloop")
+      handle1.attr("cx", handle2.attr("cx"));
+    } else {
+      handle1.attr("cx", xFirst(d));
+    }  
+  
+    //console.log("hue", " cx: ",xYearFirst(h),", year:",h);
   }
+
+function moveSlideLast(d){
+  
+  if (handle1.attr("cx") > xFirst(d)) {
+      handle2.attr("cx", handle1.attr("cx"));
+    } else {
+      handle2.attr("cx", xFirst(d));
+    } 
 }
