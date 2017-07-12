13e7357b5e3416cc5745d59cbe40cc4158537af2
tomarashish <aashish.dyp@gmail.com>
2017-07-12 11:07:02 +0200

    fix sliding problem

--- src/slider.js
+++ src/slider.js
@@ -7,13 +7,15 @@ var margin = {right: 50, left: 50, top:20, bottom:50},
 
 var svgSlider = d3.select("#slider").append("svg")
       .attr("width", slideWidth)
-      .attr("height", slideHeight);
+      .attr("height", slideHeight)
+      .attr("viewBox", "0 0 700 50")
+	   .attr("preserveAspectRatio", "xMidYMid");
 
 //x axis scale for first slider handle
 var xFirst = d3.scaleLinear()
     .domain([1900, 2017])
     .range([0, sliderWidth])
-    .nice();
+    .clamp(true);
 
 //x axis scale for second slide
 var xLast = d3.scaleLinear()
@@ -37,30 +39,22 @@ sliderFirst.append("line")
   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
     .attr("class", "track-inset")
   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-    .attr("class", "track-overlay")
-    .call(d3.drag()
-          .on("start.interrupt", function() {  sliderLast.interrupt(); })
-          .on("start drag", function() {moveSlideFirst(xFirst.invert(d3.event.x)); })
-         );
+    .attr("class", "track-overlay");
 
 sliderLast.append("line")
   .attr("class", "track")
-  .attr("id", "track1")
+  .attr("id", "track2")
   .attr("x1", xLast.range()[0])
       .attr("x2", xLast.range()[1])
 .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
     .attr("class", "track-inset")
   .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
-    .attr("class", "track-overlay")
-    .call(d3.drag()
-         .on("start.interrupt", function() { sliderFirst.interrupt(); })
-        .on("start drag", function() { moveSlideLast(xLast.invert(d3.event.x)); })
-       );
+    .attr("class", "track-overlay");
 
 
 sliderFirst.insert("g", ".track-overlay")
     .attr("class", "ticks")
-    .attr("transform", "translate(0," + 18 + ")")
+    .attr("transform", "translate(10," + 20 + ")")
   .selectAll("text")
   .data(xFirst.ticks(10))
   .enter().append("text")
@@ -70,18 +64,30 @@ sliderFirst.insert("g", ".track-overlay")
     .text(function(d) { return d ; }); 
 
 sliderLast//.insert("g", ".track-overlay")
-    .attr("transform", "translate(50," + 18 + ")")
+   // .attr("transform", "translate(50," + 18 + ")")
     .data(xLast.ticks(10));
 
 var handle1 = sliderFirst.insert("circle", ".track-overlay")
     .attr("class", "handle")
+    .attr("cy", "-1px")
     .attr("cx",xFirst.range()[0])
-    .attr("r", 9);
+    .attr("r", "5px")
+    .style("cursor", "hand")
+.call(d3.drag()
+          .on("start.interrupt", function() {  sliderLast.interrupt(); })
+          .on("start drag", function() {moveSlideFirst(xFirst.invert(d3.event.x)); })
+         );
 
 var handle2 = sliderLast.insert("circle", ".track-overlay")
       .attr("class", "handle")
+      .attr("cy", "-1px")
       .attr("cx",xLast.range()[1])
-      .attr("r", 9);
+      .attr("r", "5px")
+      .style("cursor", "hand")
+       .call(d3.drag()
+         .on("start.interrupt", function() { sliderFirst.interrupt(); })
+        .on("start drag", function() { moveSlideLast(xLast.invert(d3.event.x)); })
+       );
 
 function moveSlideFirst(d) {
     if (handle2.attr("cx") < (xFirst(d))) {
@@ -90,7 +96,7 @@ function moveSlideFirst(d) {
     } else {
       handle1.attr("cx", xFirst(d));
     }  
-  
+    console.log(d)
     //console.log("hue", " cx: ",xYearFirst(h),", year:",h);
   }
 
@@ -101,4 +107,5 @@ function moveSlideLast(d){
     } else {
       handle2.attr("cx", xFirst(d));
     } 
+  console.log(d)
 }
