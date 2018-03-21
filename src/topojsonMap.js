05e9f40b14c15b1496ae8491380f9a3462105741
tomarashish <aashish.dyp@gmail.com>
2018-03-21 09:52:22 +0100

    remove cors function

--- src/topojsonMap.js
+++ src/topojsonMap.js
@@ -282,29 +282,7 @@ d3.json("./data/denmark.topo.json", function (error, map) {
 
     } //end of mouse in
 
-    function createCORSRequest(method, url) {
-      var xhr = new XMLHttpRequest();
-      if ("withCredentials" in xhr) {
 
-        // Check if the XMLHttpRequest object has a "withCredentials" property.
-        // "withCredentials" only exists on XMLHTTPRequest2 objects.
-        xhr.open(method, url, true);
-
-      } else if (typeof XDomainRequest != "undefined") {
-
-        // Otherwise, check if XDomainRequest.
-        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-
-      } else {
-
-        // Otherwise, CORS is not supported by the browser.
-        xhr = null;
-
-      }
-      return xhr;
-    } //end of createCORSRequest
 
     function mouseOut() {
 
